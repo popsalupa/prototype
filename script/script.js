27f45dc42fe2c4120cdd546ad94789063fc2bae7
popsalupa <dimapopsa@gmail.com>
2022-12-15 16:27:08 +0300

    mini fix

--- script/script.js
+++ script/script.js
@@ -1,32 +1,3 @@
-const hasTooltips = document.querySelectorAll('.has_tooltip');
-const tooltip = document.createElement('div');
-
-Array.from(hasTooltips).forEach((element) => {
-    element.addEventListener('click', (e) => {
-    e.preventDefault();
-    
-    if (tooltip.classList.contains('tooltip_active') && tooltip.textContent === element.title) {
-        tooltip.classList.toggle('tooltip_active');
-        return;
-    }
-
-    if (document.querySelector('.active') !== null) {
-        document.querySelector('.active').classList.remove('.active');
-    }
-
-    const top = element.getBoundingClientRect().top +1;
-    const right = element.getBoundingClientRect().right;
-
-    tooltip.textContent = element.title;
-    tooltip.classList.add('tooltip_active', 'tooltip');
-    tooltip.style.right = `${right}px`;
-    tooltip.style.top = `${top}px`;
-
-    element.after(tooltip);
-    element.classList.add('active');
-    });
-  });
-
 const anim = document.querySelector('.summer');
 
 anim.onmouseover = function () {
