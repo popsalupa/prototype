4445f7687eb2ce1a2b176b367576331fe32456bd
popsalupa <dimapopsa@gmail.com>
2022-12-21 21:56:52 +0300

    hz

--- script/script.js
+++ script/script.js
@@ -1,10 +1,6 @@
-const anim = document.querySelector('.summer');
-
-anim.onmouseover = function () {
-    this.style.fontSize = '4rem';
-    this.style.color = 'pink'; 
-};
-anim.onmouseout = function () {
-    this.style.fontSize = '3.5rem';
-    this.style.color = 'black';
-};
+$(function() {
+    setTimeout(function() {
+      $('.summer').removeClass('hidden');
+    }, 600);
+ });
+    
